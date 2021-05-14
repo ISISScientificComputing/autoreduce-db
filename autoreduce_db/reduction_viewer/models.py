c620e9ae693f146e504c3aa2ddb8ea0fae891815
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-05-14 11:37:45 +0100

    Remove dependency on queue_processor STATUS

--- autoreduce_db/reduction_viewer/models.py
+++ autoreduce_db/reduction_viewer/models.py
@@ -35,7 +35,7 @@ class Instrument(models.Model):
             runs = self.reduction_runs.all()
 
         # try to get the last non-skipped run
-        last_run = runs.exclude(status=STATUS.get_skipped()).last()
+        last_run = runs.exclude(status=Status.get_skipped()).last()
         if not last_run:
             # if no non-skipped runs exist, just return the last skipped one
             last_run = runs.last()
@@ -59,6 +59,7 @@ class Status(models.Model):
     """
     Enum table for status types of messages
     """
+    _cached_statuses = {}
     STATUS_CHOICES = (('q', 'Queued'), ('p', 'Processing'), ('s', 'Skipped'), ('c', 'Completed'), ('e', 'Error'))
 
     value = models.CharField(max_length=1, choices=STATUS_CHOICES)
@@ -75,42 +76,44 @@ class Status(models.Model):
         """
         return self.value_verbose()
 
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self._cached_statuses = {}
-
-    def _get_status(self, status_value: str):
+    @staticmethod
+    def _get_status(status_value: str):
         """
         Attempt to get a status matching the given name or create one if it
         doesn't yet exist
         :param status_value: The value of the status record in the database
         """
-        if status_value in self._cached_statuses:
-            return self._cached_statuses[status_value]
+        if status_value in Status._cached_statuses:
+            return Status._cached_statuses[status_value]
         else:
-            status_record = self.objects.get_or_create(value=status_value)[0]
-            self._cached_statuses[status_value] = status_record
+            status_record = Status.objects.get_or_create(value=status_value)[0]
+            Status._cached_statuses[status_value] = status_record
         return status_record
 
-    def get_error(self):
+    @staticmethod
+    def get_error():
         """Gets the error status"""
-        return self._get_status('e')
+        return Status._get_status('e')
 
-    def get_completed(self):
+    @staticmethod
+    def get_completed():
         """Gets the completed status"""
-        return self._get_status('c')
+        return Status._get_status('c')
 
-    def get_processing(self):
+    @staticmethod
+    def get_processing():
         """Gets the processing status"""
-        return self._get_status('p')
+        return Status._get_status('p')
 
-    def get_queued(self):
+    @staticmethod
+    def get_queued():
         """Gets the queued status"""
-        return self._get_status('q')
+        return Status._get_status('q')
 
-    def get_skipped(self):
+    @staticmethod
+    def get_skipped():
         """Gets the skipped status"""
-        return self._get_status('s')
+        return Status._get_status('s')
 
 
 class Software(models.Model):
