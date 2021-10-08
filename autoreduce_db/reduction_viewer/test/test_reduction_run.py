f7c815109bcf7d70d429cc4816a5f26964b0b858
Dagonite <up821535@myport.ac.uk>
2021-10-08 23:36:24 +0000

    Fix docstrings

--- autoreduce_db/reduction_viewer/test/test_reduction_run.py
+++ autoreduce_db/reduction_viewer/test/test_reduction_run.py
@@ -1,12 +1,18 @@
+# ############################################################################### #
+# Autoreduction Repository : https://github.com/ISISScientificComputing/autoreduce
+#
+# Copyright &copy; 2021 ISIS Rutherford Appleton Laboratory UKRI
+# SPDX - License - Identifier: GPL-3.0-or-later
+# ############################################################################### #
+"""Tests for the ReductionRun class."""
+# pylint:disable=no-member
 from django.test import TestCase
 
 from autoreduce_db.reduction_viewer.models import ReductionRun
 
 
 class TestReductionRun(TestCase):
-    """
-    Directly tests the message handling classes
-    """
+    """Directly tests the message handling classes."""
     fixtures = [
         "autoreduce_db/autoreduce_django/fixtures/status_fixture.json",
         "autoreduce_db/autoreduce_django/fixtures/run_with_multiple_variables.json"
@@ -17,20 +23,20 @@ class TestReductionRun(TestCase):
         return super().setUp()
 
     def test_title(self):
-        """Test that retrieving the status returns the expected one"""
+        """Test that retrieving the status returns the expected one."""
         assert self.reduction_run.title() == "123456 - This is the test run_description"
 
     def test_title_multiple_run_numbers(self):
-        """Test that retrieving the status returns the expected one"""
+        """Test that retrieving the status returns the expected one."""
         self.reduction_run.run_numbers.create(run_number=123457)
         assert self.reduction_run.title() == "Batch 123456 → 123457 - This is the test run_description"
 
     def test_run_number(self):
-        """Test that retrieving the status returns the expected one"""
+        """Test that retrieving the status returns the expected one."""
         assert self.reduction_run.run_number == 123456
 
     def test_run_number_multiple_run_numbers(self):
-        """Test that retrieving the status returns the expected one"""
+        """Test that retrieving the status returns the expected one."""
         self.reduction_run.run_numbers.create(run_number=123457)
         with self.assertRaises(ValueError):
             self.reduction_run.run_number
